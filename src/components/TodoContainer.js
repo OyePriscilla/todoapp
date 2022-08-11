06fc3fc5e4a60d328351a8b75989986113ac026a
Oyebade Priscilla <oyebadepriscilla22@gmail.com>
2022-08-11 08:54:56 +0100

    Add file to store todo data

---
+++ src/components/TodoContainer.js
@@ -0,0 +1,92 @@
+import React from "react";
+import Header from "./Header";
+import InputTodo from "./InputTodo";
+import TodosList from "./TodosList";
+
+
+import { v4 as uuidv4 } from "uuid";
+
+class TodoContainer extends React.Component {
+  state = {
+    todos: []
+  };
+
+  handleChange = (id) => {
+    this.setState((prevState) => ({
+      todos: prevState.todos.map((todo) => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            completed: !todo.completed,
+          };
+        }
+        return todo;
+      }),
+    }));
+  };
+
+  handleDelete = (id) => {
+    this.setState({
+      todos: [
+        ...this.state.todos.filter((todo) => {
+          return todo.id !== id;
+        }),
+      ],
+    });
+  };
+
+  addTodoItem = (title) => {
+    const newTodo = {
+      id: uuidv4(),
+      title: title,
+      completed: false,
+    };
+    this.setState({
+      todos: [...this.state.todos, newTodo],
+    });
+  };
+
+  updateEditedTodo = (updatedTitle, id) => {
+    this.setState({
+      todos: this.state.todos.map((todo) => {
+        if (todo.id === id) {
+          todo.title = updatedTitle;
+        }
+        return todo;
+      }),
+    });
+  };
+
+  componentDidMount() {
+    const temp = localStorage.getItem("todos")
+    const loadedTodos = JSON.parse(temp)
+    if (loadedTodos) {
+      this.setState({
+        todos: loadedTodos
+      })
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if(prevState.todos !== this.state.todos) {
+      const temp = JSON.stringify(this.state.todos)
+      localStorage.setItem("todos", temp)
+    }
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <InputTodo addTodoProps={this.addTodoItem} />
+        <TodosList
+          todos={this.state.todos}
+          handleDeleteProps={this.handleDelete}
+          handleChangeProps={this.handleChange}
+          handleEditing={this.updateEditedTodo}
+        />
+      </div>
+    );
+  }
+}
+
+export default TodoContainer;
