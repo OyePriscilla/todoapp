9e1e9aeb9b19d005682a679d482d9093aed0f0fc
Oyebade Priscilla <oyebadepriscilla22@gmail.com>
2022-08-11 08:51:45 +0100

    Add functionality to input todo text

---
+++ src/components/InputTodo.js
@@ -0,0 +1,52 @@
+import React, { Component } from "react";
+
+class InputTodo extends Component {
+  state = {
+    title: "",
+  };
+
+  onChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    if (this.state.title.trim()) {
+      this.props.addTodoProps(this.state.title);
+      this.setState({
+        title: "",
+      });
+    }
+  };
+
+  render() {
+    return (
+      <form
+        onSubmit={this.handleSubmit}
+        style={{ border: "3px solid black", borderRadius: "20px", width: "90%", margin: "0 auto", textAlign: "center" }}
+      >
+        <input
+          style={{
+            width: "80%",
+            height: "45px",
+            border: "none",
+            fontSize: "30px",
+            fontWeight: "300"
+
+          }}
+          className="inputTodo"
+          name="title"
+          value={this.state.title}
+          onChange={this.onChange}
+          type="text"
+          placeholder="Add new Todo "
+        />
+        <button style={{backgroundColor: "inherit", border: "none", fontSize: "20px", cursor: "pointer" }}>Submit</button>
+      </form>
+    );
+  }
+}
+
+export default InputTodo;
